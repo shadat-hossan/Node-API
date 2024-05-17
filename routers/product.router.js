535c149a4623092a735296fb738a93de6a09fff1
shadat-hossan <shadathossan3500@gmail.com>
2024-05-18 00:36:47 +1200

    addes comante the route file

--- routers/product.router.js
+++ routers/product.router.js
@@ -2,11 +2,15 @@ const router = require("express").Router();
 
 const {createProduct, getProduct, getSingalProduct, updateProduct, deleteProduct} = require("../controllars/product.controllars.js")
 
-
+// Create a Product
 router.post("/", createProduct);
+// Get all Product
 router.get("/", getProduct);
+// Get a Singal Product
 router.get("/:id", getSingalProduct);
+// Update a Product
 router.put("/:id", updateProduct);
+// Delete a Product
 router.delete("/:id", deleteProduct);
 
 
